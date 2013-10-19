58db0e1a3267d39edeb413aad5462c3013a73981
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-10-20 00:43:19 +0200

    add tasks index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,4 +44,16 @@ module ApplicationHelper
     date.strftime('%d %B %Y')
   end
 
+  def vertical_menu_icon_link(title, icon, url, options = {})
+    link_to url, options.merge(class: 'item') do
+      content_tag(:i, "", class: "icon #{icon}") + title
+    end
+  end
+
+  def ui_icon_only_link(icon, url, options = {})
+    link_to url, options do
+      content_tag(:i, "", class: "icon #{icon}")
+    end
+  end
+
 end
