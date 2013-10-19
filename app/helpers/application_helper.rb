5d12f13b0616284a7c9c4c011c4f9c09af4fe300
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-10-19 15:59:11 +0200

    fix signin and logout links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def ui_button(title, link, button_options, options = {})
+    link_to title, link, options.merge(class: "ui " + button_options + " button")
+  end
+
 end
