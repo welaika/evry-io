6057920f860b5b93fbc72373dc89b95881a9abb4
Ju Liu <ju.liu@welaika.com>
2013-10-19 13:23:15 +0200

    completed create task repository

--- app/repositories/create_task.rb
+++ app/repositories/create_task.rb
@@ -2,10 +2,21 @@ class CreateTask < CreateRepository
   def after_create_hooks
     [ AddRecurrence ]
   end
+end
 
-  class AddRecurrence < Struct.new(:task)
-    def perform
-
+class AddRecurrence < Struct.new(:task)
+  def perform
+    if result = Tickle.parse(task.time_expression)
+      Recurrence.create(
+        task_id: task.id,
+        expression: result.expression,
+        starting_at: result.starting,
+        next_at: result.next,
+        until_at: result.until
+      )
+    else
+      task.errors.add :time_expression,
+        I18n.t('activerecord.errors.models.task.attributes.time_expression.parse_failed')
     end
   end
 end
