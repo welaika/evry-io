b0a36f5945a9a354a274ebff121e8880777852af
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-10-19 16:46:30 +0200

    completed home page layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
 
   def ui_button(title, link, button_options, options = {})
-    link_to title, link, options.merge(class: "ui " + button_options + " button")
+    link_to title, link, options.merge(class: "ui #{button_options} button")
+  end
+
+  def ui_icon_button(title, css_class, link, button_options, options = {})
+    link_to link, options.merge(class: "ui " + button_options + " button") do
+      content_tag(:i, "", class: "icon #{css_class}") + title
+    end
   end
 
 end
