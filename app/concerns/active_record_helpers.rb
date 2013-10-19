fc613ae5bc635fe845973efff1e0f29551943f69
Ju Liu <ju.liu@welaika.com>
2013-10-20 01:33:30 +0200

    update omniauth logic

--- app/concerns/active_record_helpers.rb
+++ app/concerns/active_record_helpers.rb
@@ -7,7 +7,14 @@ module ActiveRecordHelpers
   module ClassMethods
     def find_for_oauth(auth)
       record = where(provider: auth.provider, uid: auth.uid.to_s).first
-      record || create(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: Devise.friendly_token[0,20])
+
+      record || (
+        if existing_user = where(email: auth.info.email).first
+          existing_user.tap { |u| u.update_attributes(provider: auth.provider, uid: auth.uid) }
+        else
+          create(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: Devise.friendly_token[0,20])
+        end
+      )
     end
   end
 end
