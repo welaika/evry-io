095394a9fe204bb54bf84f89623d52645a3bfc03
Ju Liu <ju.liu@welaika.com>
2013-10-20 16:50:33 +0200

    try removing timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :save_user_time_zone, if: :current_user
-  around_filter :set_user_time_zone, if: :current_user
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = "You are not allowed to see that"
@@ -13,9 +12,5 @@ class ApplicationController < ActionController::Base
     if cookies["user_time_zone"].present? && current_user.time_zone.blank?
       current_user.update_attribute(:time_zone, cookies["user_time_zone"])
     end
-  end
-
-  def set_user_time_zone(&block)
-    Time.use_zone(current_user.time_zone || 'UTC', &block)
   end
 end
