63e00bd3201029d3f26004dc89d6f33304690dea
Matteo Piotto <matteo.piotto@welaika.com>
2013-10-20 19:36:59 +0200

    microfix on github permissions

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -219,7 +219,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, 'a517d93b502b8c32e57a', 'd164624b983583b77dab9e55efea92b9ed8b5bfb', :scope => 'user,public_repo'
+  config.omniauth :github, 'a517d93b502b8c32e57a', 'd164624b983583b77dab9e55efea92b9ed8b5bfb', :scope => 'user:email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
